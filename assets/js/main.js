7f0a40b48c46c7c7369ede5b4cf97741cd2744b5
Miguel-D-M <92712346+Miguel-D-M@users.noreply.github.com>
2022-06-06 23:11:22 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    new Typed('.typed', {
+      strings:["Hi,", "Bonjour,"],
+        loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  })
+
