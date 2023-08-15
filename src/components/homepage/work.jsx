610d4729713b1b46c144914b172e0c3078c6cb75
Miguel-D-M <miguelmariesainte@gmail.com>
2023-08-15 05:11:01 +0200

    new website

---
+++ src/components/homepage/work.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles/work.css";
+
+const Work = (props) => {
+	const { title, description, date, stack, logo } = props;
+	return (
+		<React.Fragment>
+			<div className="homepage-work">
+				<div className="homepage-work-content">
+					<div className="homepage-work-header">
+						<div className="homepage-work-logo">
+							<img src={logo} alt="logo" />
+						</div>
+						<div className="homepage-work-title">{title}</div>
+					</div>
+					<div className="homepage-work-date">
+						|&nbsp;&nbsp;&nbsp;{date}
+					</div>
+					<div className="homepage-work-description">
+						{description}
+					</div>
+					<div className="homepage-work-stack">
+						{stack}
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Work;
---
+++ src/data/seo.js
@@ -0,0 +1,11 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Software engineer with a passion for thoughtful and well elaborate code.",
+		keywords: ["Miguel", "Miguel M", "Miguel Marie-Sainte"],
+	}
+
+];
+
+export default SEO;
